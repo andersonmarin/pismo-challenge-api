db648235faacb89fd38c524f64373bc5bab6f442
Anderson <anderson.marin@outlook.com>
2021-03-19 10:32:18 -0300

    Ajuste no docker e banco

--- infrastructure/database.go
+++ infrastructure/database.go
@@ -1,6 +1,7 @@
 package infrastructure
 
 import (
+	"fmt"
 	"os"
 
 	"gorm.io/driver/postgres"
@@ -8,7 +9,14 @@ import (
 )
 
 func Database() *gorm.DB {
-	dsn := os.Getenv("DATABASE_URL")
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("DATABASE_HOST"),
+		os.Getenv("DATABASE_USER"),
+		os.Getenv("DATABASE_PASSWORD"),
+		os.Getenv("DATABASE_DBNAME"),
+		os.Getenv("DATABASE_PORT"),
+	)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
--- infrastructure/memory_database.go
+++ infrastructure/memory_database.go
@@ -1,6 +1,7 @@
 package infrastructure
 
 import (
+	"fmt"
 	"os"
 
 	"gorm.io/driver/postgres"
@@ -8,7 +9,14 @@ import (
 )
 
 func MemoryDatabase(cbs ...func(db *gorm.DB)) *gorm.DB {
-	dsn := os.Getenv("TEST_DATABASE_URL")
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable search_path=pg_temp",
+		os.Getenv("DATABASE_HOST"),
+		os.Getenv("DATABASE_USER"),
+		os.Getenv("DATABASE_PASSWORD"),
+		os.Getenv("DATABASE_DBNAME"),
+		os.Getenv("DATABASE_PORT"),
+	)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
